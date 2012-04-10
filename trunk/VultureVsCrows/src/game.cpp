44109bf929f742352fb700e05e9baf6f283ef148
janez.podhostnik@gmail.com <janez.podhostnik@gmail.com@c2b2b0e4-d568-56d6-253b-9c09bd6a759f>
2012-04-10 17:41:34 +0000

    triangle adding and deleting done... time to add physics.

--- trunk/VultureVsCrows/src/game.cpp
+++ trunk/VultureVsCrows/src/game.cpp
@@ -5,6 +5,10 @@ gmst_game::gmst_game(Font* f,Window &app):gamestate(f)
     triangle.push_back(Triangle(Vector2d(-0.1,0),Vector2d(0.0,0),Vector2d(0,0.1) ));
     triangle.push_back(Triangle(Vector2d(-0.2,1),Vector2d(-0.3,0.9),Vector2d(-0.4,0.95) ));
     editstate=EDST_nothing;
+    tn=0;
+    editbool=false;
+
+
 
 }
 
@@ -34,7 +38,24 @@ unsigned long gmst_game::Update(Window &app)
 
 
     if(!input->IsMouseButtonDown(sf::Mouse::Left))
-        editstate=EDST_nothing;
+    {
+
+        if(!(editstate==EDST_drawing))
+            editstate=EDST_nothing;
+        else if(editbool)
+        {
+
+            vtx[tn]=Vector2d(x,y);
+            tn++;
+            if(tn==3)
+            {
+                triangle.push_back(Triangle(Vector2d(vtx[0]),Vector2d(vtx[1]),Vector2d(vtx[2])));
+                editstate=EDST_nothing;
+            }
+            editbool=false;
+        }
+    }
+
     else if(editstate==EDST_moving&&input->IsMouseButtonDown(sf::Mouse::Left))
     {
         *ed=*ed+(Vector2d(x,y)-editvector);
@@ -65,11 +86,10 @@ unsigned long gmst_game::Update(Window &app)
                 editvector=Vector2d(x,y);
             }
 
-            if(editstate==EDST_nothing&&input->IsMouseButtonDown(sf::Mouse::Left))
+            if(editstate==EDST_nothing&&input->IsKeyDown(sf::Key::Delete))
             {
+                editstate=EDST_deleting;
                 ed=it;
-                editstate=EDST_moving;
-                editvector=Vector2d(x,y);
             }
 
 
@@ -81,7 +101,43 @@ unsigned long gmst_game::Update(Window &app)
 
     };
 
+    if(editstate==EDST_nothing&&input->IsMouseButtonDown(sf::Mouse::Left)&&input->IsKeyDown(sf::Key::LShift))
+    {
+        editstate=EDST_drawing;
+        tn=0;
+        editbool=true;
+    }
 
+    if(editstate==EDST_drawing&&tn>0)
+    {
+        if(input->IsMouseButtonDown(sf::Mouse::Left))
+        {
+            editbool=true;
+        }
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glColor3f(0.0,0.6,0);
+        if(tn==1)
+        {
+            glBegin(GL_LINE_STRIP);
+                glVertex2d( vtx[0].x, vtx[0].y );
+                glVertex2d( x, y );
+            glEnd();
+        }
+        else if(tn==2)
+        {
+            glBegin(GL_LINE_LOOP);
+                glVertex2d( vtx[0].x, vtx[0].y );
+                glVertex2d( vtx[1].x, vtx[1].y );
+                glVertex2d( x, y );
+            glEnd();
+        }
+
+    }
+
+    if(editstate==EDST_deleting)
+    {
+        triangle.erase(ed);
+    }
 
 
     return state;
